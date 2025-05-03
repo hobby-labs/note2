aa7718e612f3590d63f69ec95f4f4809bfe1482e
Tsutomu Nakamura <tsuna.0x00@gmail.com>
2025-05-03 12:33:16 +0900

    Updated

--- 15_React_TypeScript/react-redux-routor-thunk-multiendpoint-tailwindcss/src/components/Home.tsx
+++ 15_React_TypeScript/react-redux-routor-thunk-multiendpoint-tailwindcss/src/components/Home.tsx
@@ -5,7 +5,7 @@ type HomeProps = {
 }
 
 const Home: React.FC<HomeProps> = ({ name }) => {
-    return <h1>{name} in component.</h1>
+    return <h1 className="text-2xl">{name} in component.</h1>
 }
 
 export default Home;
--- 15_React_TypeScript/react-redux-routor-thunk-multiendpoint-tailwindcss/webpack.config.js
+++ 15_React_TypeScript/react-redux-routor-thunk-multiendpoint-tailwindcss/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -16,12 +17,23 @@ module.exports = {
         test: /\.(ts|tsx)$/, 
         exclude: /node_modules/,
         use: 'babel-loader'
-      }
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
+
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
     })
   ]
 };
+// Is needed style-loader when I use MiniCssExtractPlugin?
+// npm i -D tailwindcss style-loader css-loader postcss postcss-loader postcss-preset-env @tailwindcss/postcss mini-css-extract-plugin
